871af18502980f18190c2fee1a3b14aefa3280cc
TIEGOR01 <angelsent08@gmail.com>
2023-03-17 03:51:01 -0700

    C Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
+	char d;
+	int a;
+	long int b;
+	long long int c;
+	float f;
+
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
 	printf("Size of int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
