063a76aa126915b4d26686a6167bde12f3f3b8c3
TIEGOR01 <angelsent08@gmail.com>
2023-03-21 14:53:37 -0700

    C functions nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 0; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+					for (d = 0; d <= 9; d++)
 					{
 					_putchar(a + '0');
 					_putchar(b + '0');
