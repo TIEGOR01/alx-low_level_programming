7777999b4c84cc4e6a537e25df8efc25897355a7
TIEGOR01 <angelsent08@gmail.com>
2023-03-21 12:01:53 -0700

    C functions nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - print_alphabet, in lowercase
- *
+ * main - alphabet, in lowercase
+ * print_alphabet - Make the alphabet
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
