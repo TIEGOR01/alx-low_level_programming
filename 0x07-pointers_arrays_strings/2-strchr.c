c61f8e5f273104b53922ab78ebb97ccee39a70c3
TIEGOR01 <angelsent08@gmail.com>
2023-04-03 14:00:54 -0700

    0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,14 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] >= '\0'; i++;)
+	for (; s[i] >= '\0'; i++;)
 	{
 		if (s[i] == c)
-
-			return (s + i);
+			return (&s[i]);
 	}
-
-	return (NULL);
+	return (0);
 }
