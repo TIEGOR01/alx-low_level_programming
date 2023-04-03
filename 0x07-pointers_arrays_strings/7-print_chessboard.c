e2c20fef02de6d064b636d3da478c5fba39b0a2c
TIEGOR01 <angelsent08@gmail.com>
2023-04-03 14:33:13 -0700

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,21 +2,19 @@
 
 /**
  * Print_chessboard - Entry point
- *
  * @a: array
  *
  * Return: Always 0 (Success)
  */
 
-void print_chessboard(char (*a)[0])
+void print_chessboard(char (*a)[8])
 {
 	int i, n;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n < 0; n++)
+		for (n = 0; n < 8; n++)
 			_putchar(a[i][n]);
 		_putchar('\n');
 	}
-	return (0);
 }
