fd6bf1ff6a87dd1ed6332cfb38b0f8b55cc254ce
TIEGOR01 <angelsent08@gmail.com>
2023-04-09 09:05:51 -0700

    0x09

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,23 @@
+#!/bin/bash
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 for letters or 0 for the rest
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
