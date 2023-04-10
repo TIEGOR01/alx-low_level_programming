d7c164fe8322dd065a3d5b62cfc13e2d22e4248d
TIEGOR01 <angelsent08@gmail.com>
2023-04-10 08:08:14 -0700

    0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,24 +7,24 @@
  * @argc: number of command line arguments
  * @argv: array containing the program command line arguments
  *
- * Return: 0 or 1
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int n1 = 0, n2 = 0;
 
-	if (argc < 3 || argc > 3)
+	if (argc == 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
 
 	return (0);
 }
