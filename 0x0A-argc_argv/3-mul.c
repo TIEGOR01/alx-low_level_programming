de76bb610d35d6cf5538b28eec0f248af2e9383c
TIEGOR01 <angelsent08@gmail.com>
2023-04-10 08:40:59 -0700

    0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,29 +2,50 @@
 #include "main.h"
 
 /**
- * main - multiplies two numbers
+ * _atoi - string into integer
  *
- * @argc: number of command line arguments
- * @argv: array containing the program command line arguments
+ * @s: string
  *
- * Return: Always 0 (Success)
+ * Return: 0 or 1
  */
 
-int main(int argc, char *argv[])
+int _atoi(char *s)
 {
-	int n1 = 0, n2 = 0;
+	int i;
+	int d;
+	int n;
+	int len;
+	int f;
+	int digit;
 
-	if (argc == 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	return (0);
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
