0e95089da08fed2fff3780c103160d7cf2efa377
TIEGOR01 <angelsent08@gmail.com>
2023-04-11 13:45:06 -0700

    0x0B

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,7 +21,6 @@ char *_strdup(char *str)
 		i++;
 
 	aaa = malloc(sizeof(char) * (i + 1);
-			
 	if (aaa == NULL)
 		return (NULL);
 	for (r = 0; str[r]; r++)
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,11 +17,11 @@ int **alloc_grid(int width, int height);
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	mee = malloc(sizeof(int *) *height);
+	mee = malloc(sizeof(int *) * height);
 
 	if (mee == NULL)
 		return (NULL);
-	
+
 	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
