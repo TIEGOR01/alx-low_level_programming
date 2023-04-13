7f1cf4b92a30faa52a86df20925bedd3d1aff667
TIEGOR01 <angelsent08@gmail.com>
2023-04-13 14:41:29 -0700

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,17 +24,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[i])
 		i++;
-
 	while (s2[k])
 		k++;
-	
+
 	if (n >= k)
 		l = i + k;
 	else
 		l = i + n;
 
 	str = malloc(sizeof(char) * l + 1);
-
 	if (str == NULL)
 		return (NULL);
 
