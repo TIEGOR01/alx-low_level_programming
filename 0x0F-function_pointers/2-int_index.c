d7d3d491b7737450add7d0c5580cb530855f4284
TIEGOR01 <angelsent08@gmail.com>
2023-04-19 13:21:07 -0700

    0x0F

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
+ * int_index - returns index of the first element
  *
  * @array: array
  * @size: number of elements in the array
  * @cmp: pointer to the function to be used
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
