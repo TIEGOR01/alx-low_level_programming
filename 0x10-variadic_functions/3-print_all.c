daa6ce68d0107a36da912c896fb2959f9678471c
TIEGOR01 <angelsent08@gmail.com>
2023-04-20 11:50:55 -0700

    0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,7 +28,7 @@ void print_all(const char * const format, ...)
 					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%s%d", sep,
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
@@ -36,7 +36,7 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(list, char *);
 				if (!str)
-					str = "(nil)" :
+					str = "(nil)" ;
 						printf("%s%s", sep, str);
 					break;
 				default:
