125f228c3dc1ed348646202af64dd0c7e321d2e7
TIEGOR01 <angelsent08@gmail.com>
2023-07-05 12:38:49 -0700

    0x08

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,14 +3,33 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  *
- * @n: square root of number
+ * @n: number that returns square root
  *
  * Return: square root result
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if(n < 0)
 		return (-1);
 	return (n);
+}
+
+/**
+ * actual_sqrt_recursion - natural square root
+ *
+ * @n: number of square root
+ *
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if(i*i > n)
+		return (-1);
+	if(i*i == n)
+		return (i);
+	return (n, i + 1);
 }
